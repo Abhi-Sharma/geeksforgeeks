f6029b7cf950e4b6bda81caaf003abf9bf5ec5b3
Abhi Sharma <161916643+Abhi-Sharma@users.noreply.github.com>
2025-11-04 18:04:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort by Frequency/sort-by-frequency.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    string frequencySort(string &s) {
+        unordered_map<char,int> freq;
+        for(char ch : s){
+            freq[ch]++;
+        }
+        priority_queue<pair<int,char>, vector<pair<int,char>>, 
+        greater<pair<int,char>>> pq;
+        for(auto ch : freq){
+            pq.push({ch.second,ch.first});
+        }
+
+        string ans = "";
+        while(!pq.empty()){
+            char ch = pq.top().second;
+            int count = pq.top().first;
+            pq.pop();
+            ans.append(count,ch);
+        }
+        return ans;
+        
+    }
+};
